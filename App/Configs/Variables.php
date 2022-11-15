759efe2181f955e2c82cd2938657e7cacd3de226
zahovic Saravia Suarez <zv.elrevelde@gmail.com>
2022-11-15 16:20:07 -0500

    update gitignore

---
+++ App/Configs/Variables.php
@@ -0,0 +1,11 @@
+<?php
+define('RUTE_APP', dirname(dirname(__FILE__))); //nombre del proyecto - carpeta
+define('BASE_URL', 'http://localhost/framework-mvc-php'); //base url
+define('URI_PROJECT', '/framework-mvc-php'); //base url
+define('NAME_PROJECT', 'FRAMEWORK SIMPLE'); //base url
+define('DB_SERVER', 'localhost'); //servidor
+define('DB_NAME', 'dbsistema'); //nombre de la base de datos
+define('DB_PORT', 3306); //puerto de la base de datos
+define('DB_CHARSET', 'utf8'); //charset de la base de datos
+define('DB_USER', 'root'); //usuarios de la base de datos
+define('DB_PASS', 'root'); //contraseña de la base de datos
